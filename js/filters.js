c341cd5629acd0c4d9a23006d5c80250c88b1035
Vladislav Borovikov <borovichokv9@mail.ru>
2025-12-15 01:05:30 +0500

    module12-task1

---
+++ js/filters.js
@@ -0,0 +1,78 @@
+const RANDOM_PHOTOS_COUNT = 10;
+const DEBOUNCE_DELAY = 500;
+
+const filtersContainer = document.querySelector('.img-filters');
+const filterButtons = document.querySelectorAll('.img-filters__button');
+let currentFilter = 'filter-default';
+let debounceTimeout = null;
+
+const FilterType = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
+};
+
+const debounce = (callback, timeoutDelay = DEBOUNCE_DELAY) => {
+  return (...rest) => {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const getRandomPhotos = (photos) => {
+  const shuffledPhotos = [...photos].sort(() => Math.random() - 0.5);
+  return shuffledPhotos.slice(0, RANDOM_PHOTOS_COUNT);
+};
+
+const getDiscussedPhotos = (photos) => {
+  return [...photos].sort((a, b) => {
+    const aComments = a.comments ? a.comments.length : 0;
+    const bComments = b.comments ? b.comments.length : 0;
+    return bComments - aComments;
+  });
+};
+
+const filterPhotos = (photos, filterType) => {
+  switch (filterType) {
+    case FilterType.RANDOM:
+      return getRandomPhotos(photos);
+    case FilterType.DISCUSSED:
+      return getDiscussedPhotos(photos);
+    case FilterType.DEFAULT:
+    default:
+      return photos;
+  }
+};
+
+const onFilterChange = (callback, photos) => (evt) => {
+  const target = evt.target;
+
+  if (!target.matches('.img-filters__button')) {
+    return;
+  }
+
+  const clickedButton = target;
+
+  if (clickedButton.id === currentFilter) {
+    return;
+  }
+
+  filterButtons.forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+  });
+  clickedButton.classList.add('img-filters__button--active');
+
+  currentFilter = clickedButton.id;
+
+  const filteredPhotos = filterPhotos(photos, currentFilter);
+  callback(filteredPhotos);
+};
+
+const initFilters = (photos, renderCallback) => {
+  filtersContainer.classList.remove('img-filters--inactive');
+
+  const debouncedFilterHandler = debounce(onFilterChange(renderCallback, photos));
+  filtersContainer.addEventListener('click', debouncedFilterHandler);
+};
+
+export { initFilters };
--- js/picture-renderer.js
+++ js/picture-renderer.js
@@ -1,23 +1,39 @@
 import { openFullscreenPicture } from './fullscreen-picture.js';
 
-const renderPictures = (pictures) => {
-  const pictureTemplate = document.querySelector('#picture');
-  const picturesContainer = document.querySelector('.pictures');
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture');
 
+const clearPictures = () => {
+  if (picturesContainer) {
+    const pictureElements = picturesContainer.querySelectorAll('.picture');
+    pictureElements.forEach((element) => {
+      element.remove();
+    });
+  }
+};
+
+const renderPictures = (pictures) => {
   if (!pictureTemplate || !picturesContainer) {
     return;
   }
 
+  clearPictures();
+
   const fragment = document.createDocumentFragment();
 
   pictures.forEach((pictureData) => {
-    const { url, description, likes, comments } = pictureData;
+    const { url, description, likes, comments, id } = pictureData;
     const pictureElement = pictureTemplate.content.cloneNode(true).children[0];
+
+    if (id) {
+      pictureElement.dataset.id = id;
+    }
 
     const img = pictureElement.querySelector('.picture__img');
     if (img) {
       img.src = url;
       img.alt = description;
+      img.loading = 'lazy';
     }
 
     const likesElement = pictureElement.querySelector('.picture__likes');
@@ -41,4 +57,4 @@ const renderPictures = (pictures) => {
   picturesContainer.appendChild(fragment);
 };
 
-export { renderPictures };
+export { renderPictures, clearPictures };
