39b8754419d3df9c29f01fc391bf2f8ec33e51fc
Vladislav Borovikov <borovichokv9@mail.ru>
2025-12-27 00:16:53 +0500

    fix

--- js/filters.js
+++ js/filters.js
@@ -1,4 +1,4 @@
-import { debounce, DEBOUNCE_DELAY, shuffleArrayRandomly } from './utils.js';
+import { debounce, DEBOUNCE_DELAY, shuffleArray } from './utils.js';
 
 const RANDOM_PHOTOS_COUNT = 10;
 
@@ -12,7 +12,7 @@ const FilterType = {
 };
 
 const getRandomPhotos = (photos) => {
-  const shuffledPhotos = shuffleArrayRandomly([...photos]);
+  const shuffledPhotos = shuffleArray([...photos]);
   return shuffledPhotos.slice(0, RANDOM_PHOTOS_COUNT);
 };
 
